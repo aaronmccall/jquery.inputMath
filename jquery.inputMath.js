5b51e54f6566cbbd9f3c5ab47875fbf47f34726a
Aaron McCall <aaron@vianova.org>
2011-05-26 10:03:11 -0700

    Cleaned up uncalled code and reduced redundant variables, etc.

--- jquery.inputMath.js
+++ jquery.inputMath.js
@@ -1,44 +1,18 @@
+/*!
+ * Copyright 2010 Aaron McCall. MIT License (see http://www.opensource.org/licenses/mit-license.php)
+ * Allows performing simple arithmetic operations (including nested sub-operations) in input fields by prefixing them with
+ * the '=' sign. For example, =2+2, =10*(2-5), etc. Default behavior is to evaluate upon blur event.
+ */
 (function($){
-    $.fn.inputMath = function(options){
-//        debug(this);
-        var defaults = {
-            warnClass: 'warning',
-            event: 'blur'
-        };
-        
-        var opts = $.extend(defaults, options);
-        
-        function debug($obj){
-            if (window.console && window.console.log) {
-                if (typeof $obj == 'object') {
-                    window.console.log('element object: %o', $obj);
-                } else {
-                    window.console.log('element data: %s', $obj);
-                }
-            }
-                
-        };
-        
-        function isFloat(_str) {
-            return (_str.indexOf('.') !== -1) ? true : false;
-        }
-        
-        this.bind(opts.event, function(e){
+    $.fn.inputMath = function(event_name){
+        this.bind(event_name||'blur', function(e){
             var _val = this.value;
             if (_val.charAt(0) === '=') {
-                _regex = /([^\d\.\(\)\+\-\*\/])/g;
-                _clean = _val.replace(_regex, '');
-                debug(_clean)
-                _result = eval(_clean);
-                debug(_result)
-                if (!isNaN(_result)) {
-                    this.value =  _result;
-                }
-                    
+                //The regex strips out all characters that are not a digit, a period, a parens or an arithmetic operator
+                _result = eval(_val.replace(/([^\d\.\(\)\+\-\*\/])/g, ''));
+                if (!isNaN(_result)) this.value = _result;
             }    
-            if (opts.after) opts.after(this);
         });
-        
         return this;
     };
 })(jQuery);
